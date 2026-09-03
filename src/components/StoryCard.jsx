17cdce50c1fed24ff7192618976b7965979a8a80
francesca-golzio <francescagolzio@gmail.com>
2026-09-03 20:10:45 +0200

    update story card
    - add incipit
    - check values existence
    -

--- src/components/StoryCard.jsx
+++ src/components/StoryCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import { useStory } from "../contexts/StoryContext";
 
-export default function StoryCard({story}) {
+export default function StoryCard({ story }) {
 
+  const { getIncipit } = useStory();
 
   return (
     <>
       <div className="col-md-6 col-lg-4 my-3" key={story?.slug}>
-        <div className="card">
+        <div className="card story_card">
 
           <div className="story_tags">
             {story?.tags.map((tag) => (
@@ -14,13 +16,14 @@ export default function StoryCard({story}) {
             ))}
           </div>
 
-          <img src={story.cover_img} className="card-img-top" alt="story cover image" />
+          <img src={story?.cover_img} className="card-img-top" alt="story cover image" />
           <div className="card-body">
-            <h4 className="card-title title_font">{story.title}</h4>
-            <div className="d-flex justify-content-between">
-              <div className="card-text">{story.author.name} {story.author.surname}</div>
-              <div className="card-text">Issue {story.issue.pubblication_number}</div>
-              <Link to={`/short-stories/${story.slug}`}>read</Link>
+            <h4 className="card-title title_font">{story?.title}</h4>
+            <div className="incipit my-3">{story?.content &&getIncipit(story.content)}</div>
+            <div className="d-flex justify-content-between mt-auto">
+              <div className="card-text">by {story?.author?.name} {story?.author?.surname}</div>
+              <div className="card-text">Issue {story?.issue?.pubblication_number}</div>
+              <Link to={`/short-stories/${story?.slug}`}>read</Link>
             </div>
           </div>
         </div>
--- src/contexts/StoryContext.jsx
+++ src/contexts/StoryContext.jsx
@@ -2,22 +2,28 @@ import { createContext, useContext, useState } from "react";
 
 const StoryContext = createContext();
 
-function StoryProvider({children}) {
+function StoryProvider({ children }) {
 
   const [loading, setLoading] = useState(false);
   const [stories, setStories] = useState([]);
   const [story, setStory] = useState([]);
 
+  function getIncipit(text) {
+    const incipit = text.slice(0, 120);
+    return incipit + '...';
+  }
 
   return (
-    <StoryContext.Provider 
+    <StoryContext.Provider
       value={{
-        loading, 
-        setLoading, 
-        stories, 
-        setStories, 
-        story, 
-        setStory}}>
+        loading,
+        setLoading,
+        stories,
+        setStories,
+        story,
+        setStory,
+        getIncipit
+      }}>
       {children}
     </StoryContext.Provider>
   );
@@ -25,7 +31,7 @@ function StoryProvider({children}) {
 
 function useStory() {
   const context = useContext(StoryContext);
- 
+
   return context;
 }
 
