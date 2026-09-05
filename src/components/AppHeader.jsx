04f95efaae4d6ed60f9563ffb348a18bdc57d050
francesca-golzio <francescagolzio@gmail.com>
2026-09-05 19:27:21 +0200

    update navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -9,7 +9,7 @@ export default function AppHeader() {
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
           <div className="container gap-4">
             <NavLink to="/" className="navbar-brand"> <h1>Cup Stories</h1></NavLink>
-            <div className="navbar-text fst-italic fs-4">Small Reading Treats</div>
+            <div className="navbar-text fst-italic fs-4 text-center d-none d-lg-block w-100">Small Reading Treats</div>
             <button
               className="navbar-toggler d-lg-none"
               type="button"
@@ -23,29 +23,18 @@ export default function AppHeader() {
             <div className="collapse navbar-collapse" id="collapsibleNavId">
               <ul className="navbar-nav me-auto mt-2 mt-lg-0">
                 <li className="nav-item">
-                  <NavLink className="nav-link"  to="/" aria-current="page">Home</NavLink>
+                  <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/short-stories">Short Stories</NavLink>
+                  <NavLink className="nav-link" to="/short-stories">Stories</NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/issues">Issues</NavLink>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item disabled" aria-disabled="true">
                   <NavLink className="nav-link" to="#">Authors</NavLink>
                 </li>
               </ul>
-              <form className="d-flex my-2 my-lg-0">
-                <input
-                  className="form-control me-sm-2"
-                  type="text"
-                  placeholder="Search"/>
-                <button
-                  className="btn btn-outline-success my-2 my-sm-0"
-                  type="submit">
-                  Search
-                </button>
-              </form>
             </div>
           </div>
         </nav>
