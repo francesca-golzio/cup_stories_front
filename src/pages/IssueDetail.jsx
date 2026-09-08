9b297dc8bc2bba1c4b16105c1d3f80fad295eab8
francesca-golzio <francescagolzio@gmail.com>
2026-09-08 12:52:48 +0200

    update the issue detail page
    - fix conditional rendering: no more empty fragments when loading data

--- src/pages/IssueDetail.jsx
+++ src/pages/IssueDetail.jsx
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 export default function IssueDetail() {
 
   const endpoint = import.meta.env.VITE_API_BASE_URL;
-  const { issue, setIssue, setLoading, getYearMonth, error, setError } = useStory();
+  const { issue, setIssue, loading, setLoading, getYearMonth, error, setError } = useStory();
   const { pubblication_number } = useParams();
   const stories = issue?.stories || [];
 
@@ -45,75 +45,81 @@ export default function IssueDetail() {
 
   return (
     <>
-      {
-        error
-          ? (<p className="alert alert-secondary text-center w-50 mx-auto m-3">{error}</p>)
-          : (<div className="issue_detail_container pb-3" style={{
-            '--issue-color': issue?.color, '--issue-color-light': issue?.color + '55', '--issue-image': 'url(' + issue?.cover_img + ')'
+      {error
+        ? <p className="alert alert-secondary text-center w-50 mx-auto m-3">{error}</p>
+        : (
+          <div className="issue_detail_container pb-3" style={{
+            '--issue-color': issue?.color, '--issue-image': 'url(' + issue?.cover_img + ')'
           }}>
 
-            <Loader />
+            {loading && <Loader />}
 
-            <section className="issue_jumbo_container">
+            {(!loading && !error) && (
+              <>
+                <section className="issue_jumbo_container">
 
-              <div className="jumbo_bg_color" style={{ backgroundColor: issue?.color }}></div>
+                  <div className="jumbo_bg_color" style={{ backgroundColor: issue?.color }}></div>
 
-              <div className="issue_jumbo_contents p-4">
-                <div className="d-flex justify-content-between">
-                  <div className="issue_jumbo_date">{getYearMonth(issue?.published_at)}</div>
-                  <div className="issue_jumbo_date">Issue {issue?.pubblication_number}</div>
-                </div>
+                  <div className="issue_jumbo_contents p-4">
+                    <div className="d-flex justify-content-between">
+                      <div className="issue_jumbo_date">{getYearMonth(issue?.published_at)}</div>
+                      <div className="issue_jumbo_date">Issue {issue?.pubblication_number}</div>
+                    </div>
 
-                <h2 className="issue_jumbo_title title_font">{issue?.title}</h2>
-              </div>
+                    <h2 className="issue_jumbo_title title_font">{issue?.title}</h2>
+                  </div>
 
-            </section>
+                </section>
 
-            <div className="issue_stories_container">
+                <div className="issue_stories_container">
 
-              <h4 className="title_font mb-4">In this issue</h4>
+                  <h4 className="title_font mb-4">In this issue</h4>
 
-              <div className="container">
-                {stories.map((story) => {
-                  return (
-                    <div className="row issue_story_block my-3" key={story?.slug}>
-                      <div className="col">
-                        <Link to={`/short-stories/${story?.slug}`}>
-                          <div className="d-flex gap-3 align-items-center">
-                            <i className="bi bi-bookmark-star"></i>
-                            <div className="issue_story_title">{story?.title}</div>
-                          </div>
-                        </ Link>
-                        <div className="d-flex gap-3">
-                          <div className="issue_story_author text-muted">
-                            <small>by </small>
-                            <Link to={`/authors/${story?.author?.slug}`}>{story?.author?.name} {story?.author?.surname}</Link>
-                          </div>
-                          <div className="issue_story_tags d-flex gap-3">
-                            {story?.tags && story?.tags.map((tag) => (
-                              <div className="story_tag_container" key={tag?.label}>
-                                <div className="issue_story_tag" key={tag.label}>{tag.label}</div>
-                                <div className="story_tag_description p-3 rounded">
-                                  <div className="d-flex justify-content-between">
-                                    <h5>{tag?.name}</h5>
-                                    <small>{tag?.label}</small>
-                                  </div>
-                                  <small>{tag?.description}</small>
-                                </div>
+                  <div className="container">
+                    {stories.map((story) => {
+                      return (
+                        <div className="row issue_story_block my-3" key={story?.slug}>
+                          <div className="col">
+                            <Link to={`/short-stories/${story?.slug}`}>
+                              <div className="d-flex gap-3 align-items-center">
+                                <i className="bi bi-bookmark-star"></i>
+                                <div className="issue_story_title">{story?.title}</div>
                               </div>
-                            ))}
+                            </ Link>
+                            <div className="d-flex gap-3">
+                              <div className="issue_story_author text-muted">
+                                <small>by </small>
+                                <Link to={`/authors/${story?.author?.slug}`}>{story?.author?.name} {story?.author?.surname}</Link>
+                              </div>
+                              <div className="issue_story_tags d-flex gap-3">
+                                {story?.tags && story?.tags.map((tag) => (
+                                  <div className="story_tag_container" key={tag?.label}>
+                                    <div className="issue_story_tag" key={tag.label}>{tag.label}</div>
+                                    <div className="story_tag_description p-3 rounded">
+                                      <div className="d-flex justify-content-between">
+                                        <h5>{tag?.name}</h5>
+                                        <small>{tag?.label}</small>
+                                      </div>
+                                      <small>{tag?.description}</small>
+                                    </div>
+                                  </div>
+                                ))}
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  )
-                })}
+                      )
+                    })}
 
-              </div>
-            </div>
+                  </div>
+                </div>
+              </>
+            )}
 
-          </div>)
+          </div>
+        )
       }
+
     </>
   )
 }
