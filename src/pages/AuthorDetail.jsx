96340ae7673947e4261c013b756abd079eaa77a0
francesca-golzio <francescagolzio@gmail.com>
2026-09-08 12:43:17 +0200

    update the author detail page
    - refactor the story list appearence
    - fix conditional rendering: no more empty fragments when loading data

--- src/pages/AuthorDetail.jsx
+++ src/pages/AuthorDetail.jsx
@@ -6,7 +6,7 @@ import Loader from "../components/Loader";
 
 export default function AuthorDetail() {
 
-  const { error, setError, setLoading } = useStory();
+  const { error, setError, loading, setLoading } = useStory();
 
   const { slug } = useParams();
   const endpoint = import.meta.env.VITE_API_BASE_URL;
@@ -47,65 +47,65 @@ export default function AuthorDetail() {
   return (
     <>
 
+      {error && <p className="alert alert-secondary text-center w-50 mx-auto m-3">{error}</p>}
 
-      {
-        error
-          ? (<p className="alert alert-secondary text-center w-50 mx-auto m-3">{error}</p>)
-          : (
-            <>
-              <Loader />
+      {loading && <Loader />}
 
-
-              <div className="container author_box">
-                <div className="row justify-content-center">
-                  <div className="col col-12 col-md-8 d-flex flex-column gap-2 pt-5">
-                    <h2 className="title_font">~ {author?.name} {author?.surname} ~</h2>
-                    <h3 className="px-3">About {author?.name + ' ' + author?.surname}</h3>
-                    <p className="text-body-secondary px-3">{author?.bio}</p>
+      {(!loading && !error) &&
+        <div className="container author_box">
+          <div className="row justify-content-center">
+            <div className="col col-12 col-md-8 d-flex flex-column gap-2 pt-5">
+              <h2 className="title_font">~ {author?.name} {author?.surname} ~</h2>
+              <h3 className="px-3">About {author?.name + ' ' + author?.surname}</h3>
+              <p className="text-body-secondary px-3">{author?.bio}</p>
+            </div>
+            <div className="col col-10 col-sm-8 col-md-4 author_photo_container">
+              <img src={author?.photo} className="author_photo img-fluid" alt="" />
+            </div>
+          </div>
+          <div className="author_stories container pb-3">
+            <h3 className="mb-4">Short Stories by {author?.name + ' ' + author?.surname}</h3>
+            {author?.stories.map((story) => {
+              return (
+                <div className="row issue_story_block my-3" key={story?.slug} style={{ '--issue-color': story?.issue?.color }}>
+                  <div className="col col-9">
+                    <Link to={`/short-stories/${story?.slug}`}>
+                      <div className="d-flex gap-3 align-items-center">
+                        <i className="bi bi-bookmark-star"></i>
+                        <div className="author_story_title">{story?.title}</div>
+                      </div>
+                    </ Link>
                   </div>
-                  <div className="col col-10 col-sm-8 col-md-4 author_photo_container">
-                    <img src={author?.photo} className="author_photo img-fluid" alt="" />
+
+                  <div className="col col-2 align-items-center text-end mt-1">
+                    <Link to={`/issues/issue/${story?.issue?.pubblication_number}`}><small>Issue {story?.issue?.pubblication_number}</small></ Link>
                   </div>
-                </div>
-                <div className="author_stories container pb-3">
-                  <h3 className="mb-4">Short Stories by {author?.name + ' ' + author?.surname}</h3>
-                  {author?.stories.map((story) => {
-                    return (
-                      <div className="row issue_story_block my-3" key={story?.slug} style={{ '--issue-color': story?.issue?.color }}>
-                        <div className="col">
-                          <Link to={`/short-stories/${story?.slug}`}>
-                            <div className="d-flex gap-3 align-items-center">
-                              <i className="bi bi-bookmark-star"></i>
-                              <div className="issue_story_title">{story?.title}</div>
-                            </div>
-                          </ Link>
-                            <div className="author_story_tags d-flex gap-3">
-                              {story?.tags && story?.tags.map((tag) => (
-                                <div className="story_tag_container" key={tag?.label}>
-                                  <div className="author_story_tag">{tag.label}</div>
-                                  <div className="story_tag_description p-3 rounded">
-                                    <div className="d-flex justify-content-between">
-                                      <h5>{tag?.name}</h5>
-                                      <small>{tag?.label}</small>
-                                    </div>
-                                    <small>{tag?.description}</small>
-                                  </div>
-                                </div>
-                              ))}
-                            </div>
+
+                  <div className="col author_story_tags d-flex gap-3 mt-1">
+                    {story?.tags && story?.tags.map((tag) => (
+                      <div className="story_tag_container" key={tag?.label}>
+                        <div className="author_story_tag">{tag.label}</div>
+                        <div className="story_tag_description p-3 rounded">
+                          <div className="d-flex justify-content-between">
+                            <h5>{tag?.name}</h5>
+                            <small>{tag?.label}</small>
+                          </div>
+                          <small>{tag?.description}</small>
                         </div>
                       </div>
-                    )
-                  })}
+                    ))}
+                  </div>
 
                 </div>
+              )
+            })}
+
+          </div>
 
 
-              </div>
-
-            </>
-          )
+        </div>
       }
+
 
     </>
   )
